5d2ca2f92a5f3663f9602e3f138e49eeb31fa4d9
Guy-365 <amarnie.edwards@hgs.hiddengeniusproject.org>
2024-11-05 16:57:17 -0500

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,16 @@
 function formSubmission() {
-    // Your code here.
+   document.addEventListener("DOMContentLoaded", ){
+
+   document.getElementById("name")
+   document.getElementById("email")
+   document.getElementById("message")
+   
+   if 
+    const userName = document.getElementById('name').value;     
+    const userEmail = document.getElementById('email').value;   
+    const userMessage = document.getElementById('message').value; 
+
+    alert(`Form submitted successfully!\nName: ${userName}\nEmail: ${userEmail}\nMessage: ${userMessage}`);
+
+   });
 }
